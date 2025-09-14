e86792ec3ae207bd98f1ba6bf2fe3377a5c47ad0
Your Name <your.personal@example.com>
2025-09-13 22:49:00 -0400

    remove zoekt for now

--- bin/Register-ZoektOverlayTask.ps1
+++
@@ -1,4 +0,0 @@
-$script = "$env:USERPROFILE\bin\Watch-ZoektOverlay.ps1"
-$trigger = New-ScheduledTaskTrigger -AtLogOn
-$action  = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoLogo -ExecutionPolicy Bypass -File `"$script`""
-Register-ScheduledTask -TaskName "ZoektOverlayWatch" -Trigger $trigger -Action $action -Description "Real-time Zoekt overlay watch" -User $env:UserName -Force
